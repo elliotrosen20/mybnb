f5cdf5c71aeae47944e4d66a0858932d6a14d26d
Elliot Rosen <elliotrosen2@gmail.com>
2025-03-24 18:18:19 -0400

    Add inputs folder with input component. Created inputs components with props logic and conditional styling for input within modals.

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -13,6 +13,7 @@ import {
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import Modal from "./Modal";
 import Heading from "../Heading";
+import Input from "../inputs/Input";
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
@@ -52,6 +53,14 @@ const RegisterModal = () => {
       <Heading
         title="Welcome to Airbnb"
         subtitle="Create an account!"
+      />
+      <Input 
+        id="email"
+        label="Email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
       />
     </div>
   )
