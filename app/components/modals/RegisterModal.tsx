c2e06289bad21f763d4a86e59f8895654096f26b
Elliot Rosen <elliotrosen2@gmail.com>
2025-03-24 22:16:58 -0400

    Add log in div logic and styling in register modal.

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -92,6 +92,36 @@ const RegisterModal = () => {
         icon={FcGoogle}
         onClick={() => {}}
       />
+      <Button 
+        outline
+        label="Continue with Github"
+        icon={AiFillGithub}
+        onClick={() => {}}
+      />
+      <div
+        className="
+          text-neutral-500
+          text-center
+          mt-4
+          font-light
+        "
+      >
+        <div>
+          <div className="text-center flex flex-row items-center gap-2">
+            Already have an account?
+          </div>
+          <div
+            onClick={registerModal.onClose}
+            className="
+              text-neutral-800
+              cursor-pointer
+              hover:underline
+            "
+          >
+            Log in
+          </div>
+        </div>
+      </div>
     </div>
   )
 
