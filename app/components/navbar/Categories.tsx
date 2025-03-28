7bf9f0884892c995d251984d385be9c88c0c083c
Elliot Rosen <elliotrosen2@gmail.com>
2025-03-28 15:09:20 -0400

    Add url query logic for category component Used navigation hooks in multiple components to enable logic of which category is selected from Categories ui. Enabled styling of category boxes based on selected category.

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -3,6 +3,7 @@ import Container from "../Container";
 import { GiWindmill } from "react-icons/gi";
 import { MdOutlineVilla } from "react-icons/md";
 import CategoryBox from "../CategoryBox";
+import { usePathname, useSearchParams } from "next/navigation";
 
 export const categories = [
   {
@@ -23,6 +24,16 @@ export const categories = [
 ]
 
 const Categories = () => {
+  const params = useSearchParams();
+  const category = params?.get('category');
+  const pathname = usePathname();
+
+  const isMainPage = pathname == '/';
+
+  if (!isMainPage) {
+    return null;
+  }
+
   return (
     <Container>
       <div
@@ -39,7 +50,7 @@ const Categories = () => {
           <CategoryBox
             key={item.label}
             label={item.label}
-            description={item.description}
+            selected={category == item.label}
             icon={item.icon}
           />
         ))}
