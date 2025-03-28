39f19b394e07d995af89c93416a1d2d606b5d955
Elliot Rosen <elliotrosen2@gmail.com>
2025-03-28 11:35:42 -0400

    Add signin function to github in login modal.

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -106,12 +106,12 @@ const LoginModal = () => {
         outline
         label="Continue with Github"
         icon={AiFillGithub}
-        onClick={() => {}}
+        onClick={() => signIn('github')}
       />
       <div
         className="
           text-neutral-500
-          text-center
+          text-center 
           mt-4
           font-light
         "
