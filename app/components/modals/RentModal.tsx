1377d2b77929752365aa39c804d8b32e9f98d356
Elliot Rosen <elliotrosen2@gmail.com>
2025-03-28 19:56:46 -0400

    Add country select to rent modal. Use watch function for location (same logic for category value in first step).

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -48,6 +48,7 @@ const RentModal = () => {
   });
 
   const category = watch('category');
+  const location = watch('location');
 
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value, {
@@ -119,8 +120,9 @@ const RentModal = () => {
           title="Where is your place located?"
           subtitle="Help guests find you!"
         />
-        <CountrySelect 
-        
+        <CountrySelect
+          value={location}
+          onChange={(value) => setCustomValue('location', value)}
         />
       </div>
     )
