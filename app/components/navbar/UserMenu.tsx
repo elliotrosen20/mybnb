56f7a4d34dfe6bf55cbcea8850db1b60f468381c
Elliot Rosen <elliotrosen2@gmail.com>
2025-03-28 17:48:35 -0400

    Add enum steps with state to enable rent modal logic/ui.

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -119,7 +119,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
                   label="My properties"
                 />
                 <MenuItem
-                  onClick={() => {}}
+                  onClick={rentModal.onOpen}
                   label="Airbnb my home"
                 />
                 <hr />
