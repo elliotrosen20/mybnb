65c7df8a8a3eea191ac5104a549c49cb865f5ab0
Elliot Rosen <elliotrosen2@gmail.com>
2025-03-31 10:03:54 -0400

    Add listings route in app/api for rent modal. Used getCurrentUser function from server actions folder.

--- app/api/listings/route.ts
+++ app/api/listings/route.ts
@@ -24,4 +24,23 @@ export async function POST(
     location,
     price
   } = body;
+
+  const listing = await prisma.listing.create({
+    data: {
+      title,
+      description,
+      imageSrc,
+      category,
+      roomCount,
+      bathroomCount,
+      guestCount,
+      locationValue: location.value,
+      price: parseInt(price, 10),
+      userId: currentUser.id
+    }
+  });
+
+  return NextResponse.json(listing);
 }
+
+
